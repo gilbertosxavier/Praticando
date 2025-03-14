df807c55c427ff01522b260063435241d026d0d2
Gilberto Xavier <93664041+gilbertosxavier@users.noreply.github.com>
2025-03-14 11:46:08 -0300

    Comentando código para melhor entendimento.

--- Reactjs/requisicoes-https/src/App.jsx
+++ Reactjs/requisicoes-https/src/App.jsx
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from "react";
 import './style.css';
 
-//https://sujeitoprogramador.com/rn-api/?api=posts
+//servidor: https://sujeitoprogramador.com/rn-api/?api=posts
 
 function App() {
+
+  //Criando e inicializando vazio o array que será utilizado para renderização dos componentes na tela ;
   const [nutri, setNutri] = useState([]);
 
+  // useEffect criado para realizar a requiseção https no servidor;
   useEffect(() => {
 
     function loadApi() {
-
+      
+      //Endereço da requisição sendo armazenado em uma variável;
       let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+      //requisição sendo feita através do método fetch;
       fetch(url)
+        //transformando a promessa de resposta em um objeto json;
         .then((response) => response.json())
+        //recebendo o json como parametro atribuindo ao array setNutri;
         .then((json)=>{setNutri(json)})
     }
-
+    //função sendo invocada;
     loadApi();
-  }, []);
+  }, []);// array vazio chama a função sempre que o componente é "montado";
 
   return (
     <div className="container">
@@ -25,6 +32,7 @@ function App() {
         <strong>React Nutri</strong>
       </header>
 
+      {/* map para iterar sobre o array nutri */}
       {nutri.map((item) =>{
         return(
           <article key={item.id} className="post" >
